f0cd6aec33247bf4070fb4a5592a54a8e6157527
mason <deceitfulmason1987@gmail.com>
2026-03-22 21:28:06 -0300

    update

---
+++ introductory/variables.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(){
+
+int x;
+printf("What is the value of your variable?");
+scanf("%d", x);
+printf("The value of your variable is: ");
+printf("%d", x);
+
+return 0;
+}
